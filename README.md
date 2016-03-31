941ef834b014c591feaf0474df1770f6e15947ea
Damjan Košir <damjan.kosir@gmail.com>
2016-03-31 22:13:24 +1300

    new demo

--- README.md
+++ README.md
@@ -20,3 +20,10 @@ Use QWOP to fly (or ',RL for fellow Dvorak users)
 - display some text on the start
 - detect end of game and offer restart
 - mooooooore objectives
+- graphics... anybody willing to give me any to use?
+
+
+## DEVELOPMENT
+1. npm install typescript -g
+2. npm start
+3. (optional) npm install typings -g
--- main.ts
+++ main.ts
@@ -1,23 +1,42 @@
 /// <reference path="typings/browser.d.ts" />
 /// <reference path="./KeyState.ts" />
-var _;
-var GRAVITY = 30; //m/s^2
-var SCALE = 1; // pixels/m
-var PI = 3.14159265359;
-var SCREEN_WIDTH = 1024;
-var SCREEN_HEIGHT = 760;
+var _:any;
+
+var GRAVITY:number = 30; //m/s^2
+var SCALE:number = 1; // pixels/m
+var PI:number = 3.14159265359;
+var SCREEN_WIDTH:number = 1024;
+var SCREEN_HEIGHT:number = 760;
 var COLLISION_TYPES = {
     CIRCLE: 'circle' //has radius
 };
+
+interface QwopObject extends PIXI.DisplayObject {
+    physics?:  {
+        mass: number,
+        appliedForce: number[],
+        gravity: boolean,
+        speed: number[]
+    },
+    collision?: any,
+    animate?: (timeframe:number) => void
+}
+
+interface QwopHudObject extends PIXI.DisplayObject {
+    updateHud?: ()=> void
+}
+
 //google fonts
-var WebFontConfig = {
+var WebFontConfig:any = {
     google: {
         families: ['Snippet', 'Podkova:700']
     },
+
     active: function () {
         // do something
         //init();
     }
+
 };
 (function () {
     var wf = document.createElement('script');
@@ -28,11 +47,14 @@ var WebFontConfig = {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
 })();
+
 //game engine stuff
-function makePlayer(x, y) {
+
+function makePlayer(x:number, y:number):QwopObject {
     // create a new Sprite using the texture
     var player = new PIXI.Container();
-    var qPlayer = player;
+    var qPlayer:QwopObject = player;
+
     var body = new PIXI.Text("COPTER", {
         font: "bold 20px Podkova",
         fill: "#cc00ff",
@@ -40,6 +62,7 @@ function makePlayer(x, y) {
         stroke: "#FFFFFF",
         strokeThickness: 6
     });
+
     var leftRotor = new PIXI.Text("QWOP", {
         font: "bold 20px Podkova",
         fill: "#000000",
@@ -54,6 +77,7 @@ function makePlayer(x, y) {
         stroke: "#FFFFFF",
         strokeThickness: 1
     });
+
     body.anchor.x = 0.5;
     body.anchor.y = 1;
     leftRotor.anchor.x = 0.5;
@@ -66,12 +90,15 @@ function makePlayer(x, y) {
     leftRotor.y = -25;
     rightRotor.x = 35;
     rightRotor.y = -25;
+
     player.addChild(body);
     player.addChild(leftRotor);
     player.addChild(rightRotor);
+
     // set position
     player.x = x;
     player.y = y;
+
     //makes the object be effected by physics
     qPlayer.physics = {
         mass: 1,
@@ -79,21 +106,26 @@ function makePlayer(x, y) {
         gravity: true,
         speed: [0, 0]
     };
+
     //gives custom animation/render
     qPlayer.animate = function (frameTime) {
         //animate
         leftRotor.rotation += 0.2;
         rightRotor.rotation -= 0.2;
+
         renderer.render(player);
     };
+
     //collision attributes
     qPlayer.collision = {
         type: COLLISION_TYPES.CIRCLE,
         radius: body.width / 2
     };
+
     return player;
 }
-function makePoint(x, y) {
+
+function makePoint(x, y):QwopObject {
     var point = new PIXI.Text("POI\nNT", {
         font: "bold 20px Podkova",
         fill: "#ff0000",
@@ -101,10 +133,12 @@ function makePoint(x, y) {
         stroke: "#FFFFFF",
         strokeThickness: 6
     });
-    var qPoint = point;
+    var qPoint: QwopObject = point;
+
     point.anchor.x = point.anchor.y = 0.5;
     point.x = x;
     point.y = y;
+
     qPoint.collision = {
         type: COLLISION_TYPES.CIRCLE,
         radius: point.width / 2,
@@ -115,10 +149,12 @@ function makePoint(x, y) {
             }
         }
     };
+
     return point;
 }
+
 function makeKeyboardTrigger(keyCode) {
-    var key = {};
+    var key:any = {};
     key.code = keyCode;
     key.isDown = false;
     key.isUp = true;
@@ -127,65 +163,85 @@ function makeKeyboardTrigger(keyCode) {
     //The `downHandler`
     key.downHandler = function (event) {
         if (event.keyCode === key.code) {
-            if (key.isUp && key.press)
-                key.press();
+            if (key.isUp && key.press) key.press();
             key.isDown = true;
             key.isUp = false;
         }
         event.preventDefault();
     };
+
     //The `upHandler`
     key.upHandler = function (event) {
         if (event.keyCode === key.code) {
-            if (key.isDown && key.release)
-                key.release();
+            if (key.isDown && key.release) key.release();
             key.isDown = false;
             key.isUp = true;
         }
         event.preventDefault();
     };
+
     //Attach event listeners
-    window.addEventListener("keydown", key.downHandler.bind(key), false);
-    window.addEventListener("keyup", key.upHandler.bind(key), false);
+    window.addEventListener(
+        "keydown", key.downHandler.bind(key), false
+    );
+    window.addEventListener(
+        "keyup", key.upHandler.bind(key), false
+    );
     return key;
 }
-function add2D(a, b) {
+
+
+function add2D(a:number[], b:number[]) {
     return [a[0] + b[0], a[1] + b[1]];
 }
-function mul2D(v, s) {
+
+function mul2D(v:number[], s:number) {
     return [v[0] * s, v[1] * s];
 }
-function size2D(v) {
+
+function size2D(v:number[]) {
     return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
 }
-function div2D(v, s) {
+
+function div2D(v:number[], s:number) {
     return [v[0] / s, v[1] / s];
 }
-function init2D(rotation, size) {
+
+function init2D(rotation:number, size:number) {
     return [Math.sin(rotation) * size, Math.cos(rotation) * size];
 }
-function mirrorHorizontal2D(v) {
+
+function mirrorHorizontal2D(v:number[]) {
     return [v[0], v[1] * -1];
 }
-function mirrorVertical2D(v) {
+
+function mirrorVertical2D(v:number[]) {
     return [v[0] * -1, v[1]];
 }
-function doPhysics(object, time) {
+
+function doPhysics(object:QwopObject, time:number) {
     var acceleration = div2D(object.physics.appliedForce, object.physics.mass);
+
     if (object.physics.gravity) {
         acceleration = add2D(acceleration, [0, GRAVITY]);
     }
     object.physics.speed = add2D(object.physics.speed, mul2D(acceleration, time));
+
+
     object.physics.appliedForce = [0, 0];
     //F = m a
     //a = F/m
     //v = t a
+
     object.x += object.physics.speed[0] * SCALE;
     object.y += object.physics.speed[1] * SCALE;
+
     object.physics.speed = mul2D(object.physics.speed, 0.97);
 }
+
 function doCollision(first, second) {
     var didCollide = false;
+
     //currently we only have circular bounding boxes
     var xDiff = first.x - second.x;
     var yDiff = first.y - second.y;
@@ -194,6 +250,7 @@ function doCollision(first, second) {
     if (squareDistance <= radiusSum * radiusSum) {
         didCollide = true;
     }
+
     if (didCollide) {
         if (first.collision.onCollision) {
             first.collision.onCollision(second);
@@ -203,86 +260,110 @@ function doCollision(first, second) {
         }
     }
 }
-var MAX_ANGLE = PI / 2; //90 degrees
+
+
+var MAX_ANGLE = PI/2; //90 degrees
 var ROTATION_DECAY_FACTOR = 0.95;
+
 var ACTION_ROTATION = MAX_ANGLE / ROTATION_DECAY_FACTOR - MAX_ANGLE;
+
 function handleInput() {
     var throttle = 0.7;
+
     if (keyState.isDown('R1')) {
         player.rotation -= ACTION_ROTATION;
     }
+
     if (keyState.isDown('R2')) {
         player.rotation += ACTION_ROTATION;
     }
+
     if (keyState.isDown('L1')) {
         throttle = 2.4;
-    }
-    else if (keyState.isDown('L2')) {
+    } else if(keyState.isDown('L2')) {
         throttle = 0;
     }
+
+
     player.rotation *= ROTATION_DECAY_FACTOR;
     throttle *= GRAVITY;
     player.physics.appliedForce = add2D(player.physics.appliedForce, init2D(-player.rotation, -throttle));
 }
+
+
 //TODO get this executed after the fonts are loaded
 //gameplay
 var score = 0;
 var keyState = new KeyState();
+
 //create world
-var renderer = PIXI.autoDetectRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, { backgroundColor: 0x1099bb });
+var renderer = PIXI.autoDetectRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, {backgroundColor: 0x1099bb});
 renderer.view.style.position = 'absolute';
 renderer.view.style.left = '50%';
 renderer.view.style.top = '50%';
+
 renderer.view.style.transform = 'translate3d( -50%, -50%, 0 )';
+
 document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
-var player = makePlayer(SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+var player:QwopObject = makePlayer(SCREEN_WIDTH / 2, SCREEN_HEIGHT);
+
 stage.addChild(player);
+
 stage.addChild(makePoint(100, 100));
 stage.addChild(makePoint(100, SCREEN_HEIGHT - 100));
 stage.addChild(makePoint(SCREEN_WIDTH - 100, 100));
 stage.addChild(makePoint(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 100));
+
 //create HUD
 var scoreText = new PIXI.Text('Score: 9001 (just kidding)', {
     font: "35px Snippet",
     fill: "white",
     align: "left"
 });
-var qScoreText = scoreText;
+var qScoreText: QwopHudObject = scoreText;
 qScoreText.updateHud = function () {
     scoreText.text = "Score: " + score;
 };
 scoreText.x = 20;
 scoreText.y = 20;
-var hud = [scoreText];
+
+var hud:QwopHudObject[] = [scoreText];
 stage.addChild(scoreText);
+
 var fps = 60;
 var frameTime = 1000 / fps;
 var physicsTime = 1 / fps;
 var removeFromStage = [];
+
 gameLoop();
+
 function gameLoop() {
     setTimeout(function () {
         requestAnimationFrame(gameLoop);
+
         handleInput();
+
         //move
         _.forEach(stage.children, function (child) {
-            child = child;
+            child = <QwopObject> child;
             if (child.physics) {
                 doPhysics(child, physicsTime);
             }
         });
+
         //collision resolution
         for (var i = 0; i < stage.children.length; i++) {
-            var child = stage.children[i];
+            var child = <QwopObject>stage.children[i];
             if (child.collision) {
                 for (var j = i + 1; j < stage.children.length; j++) {
-                    var other = stage.children[j];
+                    var other = <QwopObject>stage.children[j];
                     if (other.collision) {
                         doCollision(child, other);
                     }
                 }
             }
+
             //stage walls
             if (child == player) {
                 //fix collisions
@@ -300,23 +381,26 @@ function gameLoop() {
                 }
             }
         }
+
         //remove things
         _.forEach(removeFromStage, function (childToRemove) {
             stage.removeChild(childToRemove);
         });
         removeFromStage = [];
+
         _.forEach(stage.children, function (child) {
             //animate
             if (child.animate) {
                 player.animate(frameTime);
             }
         });
+
         //update HUD
         _.forEach(hud, function (hudElemet) {
             hudElemet.updateHud();
         });
+
         //render everything
         renderer.render(stage);
     }, frameTime);
 }
-//# sourceMappingURL=main.js.map
